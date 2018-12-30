567590dcd2a1931b06ac247a1e16b98dd835858f
ktamido <ng3rdstmadgke@gmail.com>
2018-12-30 14:00:13 +0900

    expencesテーブルのマイグレーションを追加

---
+++ database/migrations/2018_12_30_135623_create_expences_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExpencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expences', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->dateTime('date');
+            $table->string('name')->nullable();
+            $table->string('category');
+            $table->index(['user_id', 'date']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expences');
+    }
+}
