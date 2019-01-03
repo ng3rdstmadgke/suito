2db90dede4567c7969cfbf6f6e82621020b88319
ktamido <ng3rdstmadgke@gmail.com>
2019-01-03 18:04:44 +0900

    ルートを全体的に見直した

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,34 @@
 |
 */
 
+/*
+*** ルートの命名規則 ***
+機能名    Controllerメソッド名 HTTPメソッド パス                        ルート名
+一覧画面  index                GET          /リソース名                 resource.index
+作成画面  create               GET          /リソース名/create          resource.create
+作成      store                POST         /リソース名                 resource.store
+詳細画面  show                 GET          /リソース名/{resource}      resource.show
+編集画面  edit                 GET          /リソース名/{resource}/edit resource.edit
+編集      update               PUT/PATCH    /リソース名/{resource}      resource.update
+削除      destroy              DELETE       /リソース名/{resource}      resource.destroy
+*/
+
 Route::get('/', function () {
     return view('welcome');
 });
 
+// 認証画面
+Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// ルート定義はIlluminate\Routing\Router::getでRouterオブジェクトになり
+// (インターフェースはIlluminate\Contracts\Routing\Registrar)
+// Illuminate\Routing\RouteCollectionに登録される
+// ※Routeがどこから来ているのかはわからない
+Route::get('/expences'               , 'ExpencesController@index');  // 一覧画面
+Route::get('/expences/create'        , 'ExpencesController@create'); // 作成画面
+Route::post('/expences'              , 'ExpencesController@store');  // 作成
+Route::get('/expences/{expence}'     , 'ExpencesController@show');   // 詳細画面
+Route::get('/expences/{expence}/edit', 'ExpencesController@edit');   // 編集画面
+Route::put('/expences/{expence}'     , 'ExpencesController@update'); // 編集
+Route::delete('/expences/{expence}'  , 'ExpencesController@destroy');// 削除
